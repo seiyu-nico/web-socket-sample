699172d7d29b70fd2e9d6fdc150a45b733e928a6
seiyu-nico <yuseiishii141@gmail.com>
2023-08-18 21:45:22 +0900

    変更

--- src-backend/config/queue.php
+++ src-backend/config/queue.php
@@ -7,8 +7,8 @@ return [
     | Default Queue Connection Name
     |--------------------------------------------------------------------------
     |
-    | Laravel's queue API supports an assortment of back-ends via a single
-    | API, giving you convenient access to each back-end using the same
+    | Laravel's queue API supports an assortment of backends via a single
+    | API, giving you convenient access to each backend using the same
     | syntax for every one. Here you may define a default connection.
     |
     */
@@ -22,7 +22,7 @@ return [
     |
     | Here you may configure the connection information for each server that
     | is used by your application. A default configuration has been added
-    | for each back-end shipped with Laravel. You are free to add more.
+    | for each backend shipped with Laravel. You are free to add more.
     |
     | Drivers: "sync", "database", "beanstalkd", "sqs", "redis", "null"
     |
